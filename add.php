36524374bc38d03570bbe1fec41dacff6bfc2a06
Ivarock <ivarssluka@gmail.com>
2024-10-02 23:16:17 +0300

    Getting data from database with mysqli

--- add.php
+++ add.php
@@ -1,30 +1,37 @@
 <?php
+$title = $email = $ingredients = '';
+
+$errors = array('email' => '', 'pizza-title' => '', 'ingredients' => '');
 
 if (isset($_POST['submit'])) {
 
     if (empty($_POST['email'])) {
-        echo 'Email is required' . '<br>';
+        $errors['email'] = 'Email is required' . '<br>';
     } else {
         $email = $_POST['email'];
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            echo "Email must be a valid email address" . '<br>';
+            $errors['email'] = "Email must be a valid email address" . '<br>';
         }
     }
     if (empty($_POST['pizza-title'])) {
-        echo 'Please enter a pizza title' . '<br>';
+        $errors['pizza-title'] = 'Please enter a pizza title' . '<br>';
     } else {
         $title = $_POST['pizza-title'];
         if (!preg_match('/^[a-zA-Z\s]+$/', $title)) {
-            echo 'Title must be letters and spaces only' . '<br>';
+            $errors['pizza-title'] = 'Title must be letters and spaces only' . '<br>';
         }
     }
     if (empty($_POST['ingredients'])) {
-        echo 'Please enter at least one ingredient';
+        $errors['ingredients'] = 'Please enter at least one ingredient';
     } else {
         $ingredients = $_POST['ingredients'];
         if (!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)) {
-            echo 'Ingredients must be a comma separated list' . '<br>';
+            $errors['ingredients'] = 'Ingredients must be a comma separated list' . '<br>';
         }
+    }
+
+    if (!array_filter($errors)) {
+        header('Location: index.php');
     }
 } // End of the POST check
 ?>
@@ -38,13 +45,16 @@ if (isset($_POST['submit'])) {
     <h4 class="center">Add a Pizza</h4>
     <form action="add.php" method="POST" class="white">
         <label for="email">Your Email:</label>
-        <input type="email" name="email" id="email">
+        <input type="email" name="email" id="email" value="<?php echo htmlspecialchars($email); ?>">
+        <div class="red-text"><?php echo $errors['email']; ?></div>
         <label for="pizza-title">Pizza Title:</label>
-        <input type="text" name="pizza-title" id="pizza-title">
+        <input type="text" name="pizza-title" id="pizza-title" value="<?php echo htmlspecialchars($title); ?>">
+        <div class="red-text"><?php echo $errors['pizza-title']; ?></div>
         <label for="ingredients">Ingredients (comma seperated):</label>
-        <input type="text" name="ingredients" id="ingredients">
+        <input type="text" name="ingredients" id="ingredients" value="<?php echo htmlspecialchars($ingredients); ?>">
+        <div class="red-text"><?php echo $errors['ingredients']; ?></div>
         <div class="center">
-            <input type="submit" name="submit" value="submit" class="waves-effect waves-light btn brand z-depth-0">
+            <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
         </div>
     </form>
 </section>
--- index.php
+++ index.php
@@ -1,7 +1,32 @@
 <?php
 
-?>
+// connect to database
+$conn = mysqli_connect('localhost', 'root', '', 'pizzas');
 
+//check connection
+if(!$conn){
+    echo 'Connection error:' . mysqli_connect_error();
+}
+
+// write a query for all pizzas
+
+$sql = 'SELECT title, ingredients, id FROM pizzas';
+
+// make query and get result
+$result = mysqli_query($conn, $sql);
+
+// fetch the resulting rows as an array
+$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+// free result from memory
+mysqli_free_result($result);
+
+// close connection
+mysqli_close($conn);
+
+print_r($pizzas);
+
+?>
 <!doctype html>
 <html lang="en">
 
